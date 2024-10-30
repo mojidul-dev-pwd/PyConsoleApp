cdb8cd33b8bfa74e7cb667fe5643447a1f3a71e3
Mojidul Islam <mojidul.islam@selisegroup.com>
2024-10-30 07:32:24 +0600

    m

--- PyCosonleApp/PyCosonleApp.py
+++ PyCosonleApp/PyCosonleApp.py
@@ -1,4 +1,4 @@
-import sys
+# import sys
 # write a comment line
 # print("hello")
 
@@ -16,6 +16,34 @@ print(name)
 num1 = 20
 num2 = 65
 print(f"My name is {name} And total mark is { num1 + num2 }")
+
+#multiline string
+multilineVariable = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(multilineVariable)
+print('\n')
+# multiline string usng '''
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
+
+# Slicing a string
+b = "Hello, World!"
+print(b[1:5])
+# Slice From the Start
+print(b[:5])
+# Slice To the End
+print(b[3:])
+print(b.upper())
+print(b.lower())
+
+#The strip() method removes any whitespace from the beginning or the end
+cc = " Hello, World! "
+print(cc.strip())
 
 #bytes is an immutable object
 numList = [1,3,5,6,255,252] #Range 0 to 255
@@ -35,4 +63,14 @@ print(type(x))
 #List declaration
 fruitList = ["apple", "banana", "cherry"]
 print(type(fruitList))
+print(len(fruitList))
+print(fruitList)
+
+list1 = ["abc", 34, True, 40, "male"]
+
+# Using the list() constructor to make a List
+fruitList2 = list(("apple", "banana", "cherry"))
+print(fruitList2)
+
+
 
