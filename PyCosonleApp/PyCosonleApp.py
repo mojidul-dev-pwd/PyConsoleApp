7bc632ab27a5784afbee511a65378fd26073a1c6
Mojidul Islam <mojidul.islam@selisegroup.com>
2024-11-16 21:24:00 +0600

    add-try-except

--- PyCosonleApp/PyCosonleApp.py
+++ PyCosonleApp/PyCosonleApp.py
@@ -691,8 +691,39 @@ print(x)
 c = camelcase.CamelCase()
 txt = "hello world"
 print(c.hump(txt))
+#try catch block
+try:
+  print(xx)
+except NameError:
+  print("Variable xx is not defined")
+except:
+  print("Something else went wrong")
 
+try:
+  print("Hello")
+except:
+  print("Something went wrong")
+else:
+  print("Nothing went wrong")
 
+try:
+  print(xx)
+except:
+  print("Something went wrong")
+finally:
+  print("The 'try except' is finished")
 
+try:
+  f = open("demofile.txt")
+  try:
+    f.write("Lorum Ipsum")
+  except:
+    print("Something went wrong when writing to the file")
+  finally:
+    f.close()
+except:
+  print("Something went wrong when opening the file")
 
-
+x = -1
+if x < 0:
+  raise Exception("Sorry, no numbers below zero")
