ec6f4c901918c3cb7097c8fb4a0349f5d086e546
Mojidul Islam <mojidul.islam@selisegroup.com>
2024-11-26 11:50:22 +0600

    add-py-ex1

--- PyCosonleApp/Basic/problem_solution.py
+++ PyCosonleApp/Basic/problem_solution.py
@@ -1,3 +1,7 @@
+import sys
+import datetime
+from math import pi
+
 #sum of 1 to 100 numbers
 num = 100
 sum = 0
@@ -19,5 +23,19 @@ for i in range(num+1):
     if i != 0 and i%2 == 0:
         e.append(i)
 print(e)
+
+#string format
+print("Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are!")
+print(sys.version)
+print(sys.version_info)
+
+now = datetime.datetime.now()
+print(now)
+print(now.strftime("%Y-%m-%d %H:%M:%S"))
+
+r = float(input("Input the radius of the circle : "))
+area = pi * r ** 2
+print("The area of the circle with radius " + str(r) + " is: " + str(area))
+
 
 
